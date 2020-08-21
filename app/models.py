e0c07b01bd816c065702baaec497ea19c60ff725
Danil <aleshaalmery@gmail.com>
2020-08-21 18:26:17 +0300

    Completed main

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='blog')
+    description = models.TextField(max_length=200000)
+    date = models.DateField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
